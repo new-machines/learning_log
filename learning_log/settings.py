d64705fee6c4ce12ceda9ec8aa0c870214723f0a
new-machines <bc@newmachines.xyz>
2020-07-14 11:54:46 -0400

    commit operation

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     # My apps
     'learning_logs',
+    'users',
 
 
     # Default django apps
@@ -123,3 +124,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# My settings
+LOGIN_URL = 'users:login'
